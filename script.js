3fff41439b100d2264df633b8f6aa2d329836c79
Lucas Vieira <85909450+LucasVieira757@users.noreply.github.com>
2023-07-17 12:03:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoProximoDia = document.getElementById("proximo-dia");
+
+botaoProximoDia.addEventListener("click", function() {
+  // Aqui você pode adicionar o código para avançar para o próximo dia
+});
